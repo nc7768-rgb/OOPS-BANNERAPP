25059a3c2c900873ece536cf9588cc1fd13e2b60
nc7768-rgb <nc7768@srmist.edu.in>
2026-03-03 09:08:23 +0530

    UC1-Printing OOPS

---
+++ OOPsBannerApp.java
@@ -0,0 +1,17 @@
+/**
+ * OOPSBannerApp
+ * 
+ * Prints the literal text "OOPS" to the console.
+ * 
+ * @author NIKHIL CHANDRAN
+ * @version 1.0
+ */
+class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
