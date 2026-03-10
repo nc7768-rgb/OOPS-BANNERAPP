e5db2962ef887edecb13d4f0ffab7313bfc9bd22
nc7768-rgb <nc7768@srmist.edu.in>
2026-03-10 08:43:17 +0530

    UC2 - Use HashMap for character

--- OOPsBannerApp.java
+++ OOPsBannerApp.java
@@ -1,17 +1,20 @@
 /**
  * OOPSBannerApp
- * 
- * Prints the literal text "OOPS" to the console.
- * 
- * @author NIKHIL CHANDRAN
- * @version 1.0
+ * Prints OOPS in large banner format
+ * @author NIKHIL CHANDRAN 
+ * @version 2.0
  */
 class OOPSBannerApp {
 
-    /**
-     * Main method - Entry point of the application
-     */
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *****   *****  ");
+        System.out.println("*     * *     * *            * ");
+        System.out.println("*     * *     * *            * ");
+        System.out.println(" *****   *****  *       ****** ");
+
     }
 }
